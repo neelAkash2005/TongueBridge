5f128f1b3db4d7d9b50b0449bead547dba669857
Akash Poddar <whoami.9321@gmail.com>
2026-02-13 20:16:19 +0530

    Initial React setup

---
+++ frontDev/frontDev/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>My React App 🚀</h1>
+      <p>Welcome! This is my first React + Vite project.</p>
+    </div>
+  );
+}
+
+export default App;
