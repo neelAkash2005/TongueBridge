4412e8a4f1857a624ed69f9828c28882165be0d5
Akash Poddar <whoami.9321@gmail.com>
2026-03-23 23:44:56 +0530

    initial backend setup

---
+++ backend/app/schemas/translation.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+class TranslationRequest(BaseModel):
+	text: str
+	source_language: str
+	target_language: str
+
+
+class TranslationResponse(BaseModel):
+	translated_text: str
